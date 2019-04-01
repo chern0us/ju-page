f3c3ff6f6a55c31b1c35286350822466d47b49f4
Nikita Belous <belousnikita@gmail.com>
2019-04-01 20:00:09 +0300

    Updates

---
+++ precache-manifest.7b97401b8eb7d2f37250da3c1890ec52.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8b092bd6670ba73fb1a187fc6ac869a",
+    "url": "/ju-page/static/media/f1.a8b092bd.png"
+  },
+  {
+    "revision": "ef2108879a8950da93da",
+    "url": "/ju-page/static/css/main.dcd6428b.chunk.css"
+  },
+  {
+    "revision": "0992288d31349843e259",
+    "url": "/ju-page/static/js/runtime~main.922cbc86.js"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/ju-page/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "972fec182f1a933f8b85190d8bc4a420",
+    "url": "/ju-page/static/media/banner.972fec18.png"
+  },
+  {
+    "revision": "932ab20b22c52fdcea443bc981f5ab0d",
+    "url": "/ju-page/static/media/apple.932ab20b.png"
+  },
+  {
+    "revision": "ef2108879a8950da93da",
+    "url": "/ju-page/static/js/main.de9cafc3.chunk.js"
+  },
+  {
+    "revision": "81f5c4496595bebc00d7425c7cc21985",
+    "url": "/ju-page/static/media/f2.81f5c449.png"
+  },
+  {
+    "revision": "11b5b73bf7dc0e164440952b4bda959e",
+    "url": "/ju-page/static/media/f3.11b5b73b.png"
+  },
+  {
+    "revision": "80e1d4214b88dd3293e90f8b9f4e208f",
+    "url": "/ju-page/static/media/f4.80e1d421.png"
+  },
+  {
+    "revision": "54965263d6116393c95b296780aed3b2",
+    "url": "/ju-page/static/media/fruits.54965263.png"
+  },
+  {
+    "revision": "b6230b2fd59a23178cccf4359106666e",
+    "url": "/ju-page/static/media/qutoes.b6230b2f.png"
+  },
+  {
+    "revision": "253b32fe20f7a03488cd47a2d8ef9699",
+    "url": "/ju-page/index.html"
+  }
+];
